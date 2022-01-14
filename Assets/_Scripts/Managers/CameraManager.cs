d5cbe91ea9f062bf730062d166636096752743bc
spencerbarck <60077342+spencerbarck@users.noreply.github.com>
2022-01-13 23:04:06 -0600

    Added Camera Manager

--- Assets/_Scripts/Managers/CameraManager.cs
+++ Assets/_Scripts/Managers/CameraManager.cs
@@ -4,9 +4,85 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Camera Camera;
+    public Transform Player;
+    private Vector3 target, mousePosition, refVel, shakeOffset;
+    private float cameraDistance = 3.5f;
+    private float smoothTime = 0.2f;
+    private float zStart;
+
+    float shakeMag, shakeTimeEnd;
+    Vector3 shakeVector;
+    bool isShaking;
+    //public Camera Camera;
     void Start()
     {
-        Camera = GetComponent<Camera>();
+        //Camera = GetComponent<Camera>();
+        target = Player.position;
+        zStart = transform.position.z;
+    }
+
+    void Update()
+    {
+        mousePosition = CaptureMousePosition();
+
+        shakeOffset = UpdateShake();
+
+        target = UpdateTargetPosition();
+
+        UpdateCameraPosition();
+    }
+
+    Vector3 CaptureMousePosition(){
+        Vector2 position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        position *=2;
+        position -= Vector2.one;
+
+        //Keep distance accurate around edges of screen
+        if(Mathf.Abs(position.x)>0.9f || Mathf.Abs(position.y)>0.9f)
+        {
+            position = position.normalized;
+        }
+        return position;
+    }
+
+    Vector3 UpdateTargetPosition()
+    {
+        Vector3 mouseOffset = mousePosition * cameraDistance;
+        Vector3 position = Player.position + mouseOffset;
+        position += shakeOffset;
+        position.z = zStart;
+        return position;
+    }
+
+    void UpdateCameraPosition()
+    {
+        Vector3 tempPosition;
+
+        tempPosition = Vector3.SmoothDamp(transform.position, target, ref refVel, smoothTime);
+
+        transform.position = tempPosition;
+    }
+
+    public void ScreenShake(Vector3 direction, float magnitude, float length)
+    {
+        isShaking = true;
+        shakeVector = direction;
+        shakeMag = magnitude;
+        shakeTimeEnd = Time.time + length;
+    }
+
+    Vector3 UpdateShake()
+    {
+        if (!isShaking || Time.time > shakeTimeEnd)
+        {
+            Debug.Log("Not Shaking");
+            isShaking = false;
+            return Vector3.zero;
+        }
+        Debug.Log("Shaking");
+
+        Vector3 tempOffset = shakeVector;
+        tempOffset *= shakeMag;
+        return tempOffset;
     }
 }
--- Assets/_Scripts/Player/PlayerShooter.cs
+++ Assets/_Scripts/Player/PlayerShooter.cs
@@ -11,6 +11,8 @@ public class PlayerShooter : MonoBehaviour
     private GameObject projectilePrefab;
     [SerializeField]
     private float projectileForce = 20f;
+    [SerializeField]
+    private CameraManager cameraManager;
     private void Update()
     {
         if(Input.GetButtonDown("Fire1"))
@@ -21,6 +23,7 @@ public class PlayerShooter : MonoBehaviour
 
     private void Shoot()
     {
+        cameraManager.ScreenShake((transform.position - firePoint.position).normalized, 1f, 0.05f);
         GameObject projectile = Instantiate(projectilePrefab,firePoint.position,firePoint.rotation);
         Rigidbody2D rigidBody = projectile.GetComponent<Rigidbody2D>();
         rigidBody.AddForce(firePoint.up * projectileForce, ForceMode2D.Impulse);
